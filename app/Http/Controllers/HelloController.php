4b72e5bb9248a6a109e06860800b7467dce9e200
datchlive <71320906+DatchLive@users.noreply.github.com>
2021-11-15 22:56:34 +0900

    3-4. レイアウトの作成 学習

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -9,14 +9,18 @@ class HelloController extends Controller
 {
     public function index()
     {
-        $data = ['one', 'two', 'three', 'four', 'five'];
+        $data = [
+        ['name'=>'山田','mail'=>'taro@gmail.com'],
+        ['name'=>'本田','mail'=>'honda@gmail.com'],
+        ['name'=>'鈴木','mail'=>'suzuki@gmail.com']
+    ];
         return view('hello.index', ['data' => $data]);
     }
 
-    public function post(Request $request)
-    {
-        return view('hello.index',['msg'=>$request->msg]);
-    }
+    // public function post(Request $request)
+    // {
+    //     return view('hello.index',['msg'=>$request->msg]);
+    // }
        
 }
 
--- resources/views/hello/index.blade.php
+++ resources/views/hello/index.blade.php
@@ -1,24 +1,33 @@
-<html>
-  <head>
-    <title>Hello/Index</title>
-    <style>
-      body { font-size:16px; color:#999; }
-      h1 { font-size:50px; text-align:right; color:#f6f6f6; margin:-20px 0px -30px 0px; letter-spacing: -4pt; }
-    </style>
-  </head>
-  <body>
-    <h1>Blade/Index</h1>
-    <p>&#064;Whileディレクティブの例</p>
-    <ol>
-    @php
-    $counter = 0;
-    @endphp
-    @while ($counter < count($data))
-    <li>{{$data[$counter]}}</li>
-    @php
-    $counter++;
-    @endphp
-    @endwhile
-    </ol>
-  </body>
-</html>
+@extends('layouts.helloapp')
+
+@section('title', 'Index')
+
+@section('menubar')
+    @parent
+    インデックスページ
+@endsection
+
+@section('content')
+  <p>ここが本文のコンテンツです。</p>
+  <ul>
+    @each('components.item', $data, 'item')
+  </ul>
+  {{-- <p>必要なだけ記述できます。</p> --}}
+
+  {{-- @component('components.message')
+  @slot('msg_title')
+    CAUTION!
+  @endslot
+
+  @slot('msg_content')
+    これはメッセージの表示です。
+  @endslot
+  @endcomponent --}}
+
+  @include('components.message', ['msg_title'=>'OK', 'msg_content'=>'サブビューです。'])
+
+@endsection
+
+@section('footer')
+copyright 2019 tuyano.
+@endsection

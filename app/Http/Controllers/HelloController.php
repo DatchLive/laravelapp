a8dcb957a2bff22eed6906042f55a98615f70563
datchlive <71320906+DatchLive@users.noreply.github.com>
2021-11-16 22:55:21 +0900

    3-5.サービスとビューコンポーザ 学習

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -9,18 +9,7 @@ class HelloController extends Controller
 {
     public function index()
     {
-        $data = [
-        ['name'=>'山田','mail'=>'taro@gmail.com'],
-        ['name'=>'本田','mail'=>'honda@gmail.com'],
-        ['name'=>'鈴木','mail'=>'suzuki@gmail.com']
-    ];
-        return view('hello.index', ['data' => $data]);
+        return view('hello.index', ['message' => 'Hello!']);
     }
-
-    // public function post(Request $request)
-    // {
-    //     return view('hello.index',['msg'=>$request->msg]);
-    // }
-       
 }
 

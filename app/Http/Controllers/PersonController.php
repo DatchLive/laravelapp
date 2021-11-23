0052ec9ac163bd793044975b2d61d8694bae2dd0
datchlive <71320906+DatchLive@users.noreply.github.com>
2021-11-23 17:36:34 +0900

    6-1. Eloquentの基本 学習

---
+++ app/Http/Controllers/PersonController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Person;
+use Illuminate\Http\Request;
+
+class PersonController extends Controller
+{
+    public function find(Request $request)
+    {
+        return view('person.find', ['input' => '']);
+    }
+
+    public function search(Request $request)
+    {
+      $item = Person::find($request->input);
+      $param = ['input' => $request->input, 'item' => $item];
+      return view('person.find', $param);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HelloController;
+use App\Http\Controllers\PersonController;
 use App\Http\Middleware\HelloMiddleware;
 
 /*
@@ -45,3 +46,6 @@ Route::post('hello/edit',[HelloController::class,'update']);
 Route::get('hello/del',[HelloController::class,'del']);
 Route::post('hello/del',[HelloController::class,'remove']);
 Route::get('hello/show',[HelloController::class,'show']);
+Route::get('person', [PersonController::class, 'index']);
+Route::get('person/find', [PersonController::class, 'find']);
+Route::get('person/find', [PersonController::class, 'search']);
